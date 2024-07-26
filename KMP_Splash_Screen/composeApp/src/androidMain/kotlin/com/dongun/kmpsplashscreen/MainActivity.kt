3eb09f632af3c2e1adceb22706f6ebb673621094
kdu0136 <kdu0136@naver.com>
2024-07-26 10:49:35 +0900

    fear: add splash screen android/iOS platform

---
+++ KMP_Splash_Screen/composeApp/src/androidMain/kotlin/com/dongun/kmpsplashscreen/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.dongun.kmpsplashscreen
+
+import App
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                false
+            }
+        }
+        setContent {
+            App()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
